312e0d070d78c6732612f4d2a8120c160ce18f22
KatIsCoding <fabrigoru@gmail.com>
2021-10-11 12:32:59 -0600

    Webpack Setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+module.exports = {
+  entry: './src/index.js',
+  mode: "development",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+      title: "Music",
+      template: "./src/index.html"
+  })],
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    },
+};
