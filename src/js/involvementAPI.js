1ed920ce5e495475d9045ec1320894cfa4d372f5
mike2611 <mpuentes2611@gmail.com>
2021-10-13 13:40:12 -0500

    Add involvementAPI.js file

---
+++ src/js/involvementAPI.js
@@ -0,0 +1,21 @@
+import { APIKEY } from './api.js';
+
+const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const commentsURL = `${baseURL}/apps/${APIKEY}/comments`;
+
+export const postComment = (id, userName, userText) => {
+  const comment = {
+    item_id: id,
+    username: userName,
+    comment: userText,
+  };
+  const response = fetch(commentsURL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(comment),
+  });
+  return response;
+};
