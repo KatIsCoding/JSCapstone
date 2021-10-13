f6f8a6cd21ef3a94658515439f730f65d08b4323
KatIsCoding <fabrigoru@gmail.com>
2021-10-12 21:01:43 -0600

    Solved bug of cards not loading when localStorage initializes empty

--- src/js/spotifyAPI.js
+++ src/js/spotifyAPI.js
@@ -1,27 +1,28 @@
 import { SPOTIFYKEY } from "./crypto.js";
 export class Spotify {
   constructor() {
-    this.renewKey()
+    //this.renewKey()
   }
 
   renewKey = async () => {
     if (localStorage.getItem("lastKeyTime") != null){
+        // Gotta return a promise so the function doesn't get inmediately resolved
         if ((((Date.now() - parseInt(localStorage.getItem("lastKeyTime")))))/60 > 3600){
-            this.getApiKey().then(api => {
+            return this.getApiKey().then(api => {
                 localStorage.setItem("lastKey", api["access_token"])
+                localStorage.setItem("lastKeyTime", Date.now()) 
             })
-            localStorage.setItem("lastKeyTime", Date.now()) 
         }
     } else {
-        this.getApiKey().then(api => {
+        return this.getApiKey().then(api => {
             localStorage.setItem("lastKey", api["access_token"])
-            console.log("Api Key Renewed!!", this.apiKey)})
-        localStorage.setItem("lastKeyTime", Date.now())
+            localStorage.setItem("lastKeyTime", Date.now())
+            console.log("Api Key Renewed!!", localStorage.lastKey)}) 
     }
 
   }
 
-  getApiKey = async () => {
+  getApiKey = () => {
       return fetch("https://accounts.spotify.com/api/token", {
           method: "POST",
           headers: {
@@ -34,6 +35,7 @@ export class Spotify {
 
   getAlbum = async (albumID) => {
     await this.renewKey()
+    
     return fetch("https://api.spotify.com/v1/albums/"+albumID, {
         headers: {
             Accept: "application/json",
@@ -42,21 +44,22 @@ export class Spotify {
         }
     }).then(r => r.json()).then(d => {return d})
   }
-  get50Albums = async() => {
+  get50Albums = async () => {
+      // Since a promise is being returned properly, I can await the fullfillment of the promise
       await this.renewKey()
-      return fetch("https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF", {
-          headers: {
-              Accept: "application/json",
-              "Content-type": "applicacion/json",
-              Authorization: "Bearer "+localStorage.getItem("lastKey")
-          }
-      }).then(r => r.json()).then(d =>{
-          let out = []
-          d["tracks"]["items"].forEach((trackObj) => {
-            out.push(trackObj["track"]["album"])
-          })
-          console.log(out)
-          return out
-      })
+        return fetch("https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF", {
+            headers: {
+                Accept: "application/json",
+                "Content-type": "applicacion/json",
+                Authorization: "Bearer "+localStorage["lastKey"]
+            }
+        }).then(r => r.json()).then(d =>{
+            let out = []
+            d["tracks"]["items"].forEach((trackObj) => {
+              out.push(trackObj["track"]["album"])
+            })
+            console.log(out)
+            return out
+        })
   }
 }
